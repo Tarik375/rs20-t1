57886b7f48af8888306e4a6614e72b71819d9a2d
Tarik <tmekic1@etf.unsa.ba>
2021-03-23 13:49:32 +0100

    Novi commit!!!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
 public class Controller {
-}
+
+    public Label helloW;
+
+
+    public void onAction(ActionEvent actionEvent) {
+
+        helloW.setText("Hello World");
+        helloW.getStyleClass().add("crvenaa");
+
+
+
+
+    }
+
+
+
+
+
+
+ }
+
+
+
